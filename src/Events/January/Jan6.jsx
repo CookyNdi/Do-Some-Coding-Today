3093b6d45eb3954d10a88446ff92be709f5c0fa9
CookyNdi <yandikun145@gmail.com>
2023-01-06 15:08:27 +0700

    6 january 2023

--- src/Events/January/Jan6.jsx
+++ src/Events/January/Jan6.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Screenshot from "../../asset/coming.webp";
+import Screenshot from "../../asset/1-6.webp";
 
 const Jan6 = () => {
   return (
@@ -8,7 +8,7 @@ const Jan6 = () => {
         <img src={Screenshot} alt="6-1-2023" />
       </div>
       <div className="Tittle">
-        <h2>Not Coming</h2>
+        <h2>Membuat Login Form #1</h2>
       </div>
     </div>
   );
