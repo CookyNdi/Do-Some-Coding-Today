318d896cf9e0a7c17f8cf114d23845e29bd212db
CookyNdi <yandikun145@gmail.com>
2023-01-13 20:36:37 +0700

    13 January 2023

--- src/Events/January/Jan13.jsx
+++ src/Events/January/Jan13.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Screenshot from "../../asset/coming.webp";
+import Screenshot from "../../asset/bocchi.gif";
 
 const Jan13 = () => {
   return (
@@ -8,7 +8,7 @@ const Jan13 = () => {
         <img src={Screenshot} alt="13-1-2023" />
       </div>
       <div className="Tittle">
-        <h2>Not Coming</h2>
+        <h2>Dump</h2>
       </div>
     </div>
   );
