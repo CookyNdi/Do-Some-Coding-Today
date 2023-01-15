23d46aa9d38bdc528c2e0912b2fae92de9aa412d
CookyNdi <yandikun145@gmail.com>
2023-01-15 18:49:12 +0700

    15 January 2023

--- src/Events/January/Jan15.jsx
+++ src/Events/January/Jan15.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Screenshot from "../../asset/coming.webp";
+import Screenshot from "../../asset/1-15.webp";
 
 const Jan15 = () => {
   return (
@@ -8,7 +8,7 @@ const Jan15 = () => {
         <img src={Screenshot} alt="15-1-2023" />
       </div>
       <div className="Tittle">
-        <h2>Not Coming</h2>
+        <h2>DSCT : Memperbaiki Card Dan Menambah Beberapa Element #5</h2>
       </div>
     </div>
   );
