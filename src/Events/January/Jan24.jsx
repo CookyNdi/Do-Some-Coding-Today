4f49b10ae3efe499a7399a27c2a373a53a16c5bc
CookyNdi <yandikun145@gmail.com>
2023-01-24 13:44:57 +0700

    24 January 2023

--- src/Events/January/Jan24.jsx
+++ src/Events/January/Jan24.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Screenshot from "../../asset/coming.webp";
+import Screenshot from "../../asset/1-24.png";
 
 const Jan24 = () => {
   return (
@@ -8,7 +8,7 @@ const Jan24 = () => {
         <img src={Screenshot} alt="24-1-2023" />
       </div>
       <div className="Tittle">
-        <h2>Not Coming</h2>
+        <h2>Membuat Sistem CRUD Sederhana Dengan Menggunakan Google Sheet #1</h2>
       </div>
     </div>
   );
