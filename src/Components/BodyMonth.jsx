f6a2323b86910c3c8544b5a40b941f3cfdc4d822
CookyNdi <yandikun145@gmail.com>
2023-01-25 16:41:12 +0700

    25 January 2023 | Beta v0.5.11 For DSCT

--- src/Components/BodyMonth.jsx
+++ src/Components/BodyMonth.jsx
@@ -1,42 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const BodyMonth = () => {
-  const sheetId = "10r-IFwefCFvFDGDc1cSVHwq-P9-RnJ63Vgr2aHoxKw4";
-  const baseUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
-  const sheetName = "January";
-  const qu = "Select *";
-  // const qu = "Select * WHERE B = 2";
-  const query = encodeURIComponent(qu);
-  const url = `${baseUrl}&sheet=${sheetName}&tq=${query}`;
-  const data = [];
-
   const [datas, setData] = useState([]);
   useEffect(() => {
-    db();
-  });
+    getDatas();
+  }, []);
 
-  const db = () => {
-    fetch(url)
-      .then((res) => res.text())
-      .then((rep) => {
-        const jsData = JSON.parse(rep.substr(47).slice(0, -2));
-        // console.log(jsData);
-        const colz = [];
-        jsData.table.cols.forEach((heading) => {
-          if (heading.label) {
-            colz.push(heading.label.toLowerCase().replace(/\s/g, ""));
-          }
-        });
-        jsData.table.rows.forEach((main) => {
-          const row = {};
-          colz.forEach((element, index) => {
-            row[element] = main.c[index] != null ? main.c[index].v : "NULL";
-          });
-          data.push(row);
-        });
-        setData(data);
-      });
+  const getDatas = async () => {
+    const response = await axios.get("http://localhost:5000/january");
+    if (response.data !== undefined && response.data !== null) {
+      setData(response.data);
+    }
   };
 
   return (
